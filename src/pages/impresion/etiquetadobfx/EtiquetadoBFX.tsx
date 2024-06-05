c158a2dcaf88a451dcf076c79e4202eff3fdfba7
EzekielJimenezO <ejo73635@udelasalle.edu.mx>
2024-06-05 09:30:41 -0600

    Actualizacion para que se pueda mostrar la modal en formato

--- src/pages/impresion/etiquetadobfx/EtiquetadoBFX.tsx
+++ src/pages/impresion/etiquetadobfx/EtiquetadoBFX.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Box, Button, IconButton, MenuItem, Select, TextField, Typography, Snackbar, Alert } from '@mui/material';
+import { Box, Button, IconButton, MenuItem, Select, TextField, Typography, Modal, Paper,  } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import CloseIcon from '@mui/icons-material/Close';
 import './etiquetadobfx.scss';
-
+import EtiquetaImpresion from '../../../assets/EiquetBFX.jpg';
 interface Area {
   id: number;
   area: string;
@@ -58,6 +59,8 @@ const EtiquetadoBFX: React.FC = () => {
   const [pesoTarima, setPesoTarima] = useState<number | undefined>();
   const [piezas, setPiezas] = useState<number | undefined>();
   const [selectedOperador, setSelectedOperador] = useState<number | undefined>();
+  const [openModal, setOpenModal] = useState(false);
+  const [currentDate, setCurrentDate] = useState<string>('');
 
   useEffect(() => {
     // Carga inicial de áreas y turnos
@@ -101,49 +104,60 @@ const EtiquetadoBFX: React.FC = () => {
         .catch(error => console.error('Error fetching data:', error));
     }
   }, [selectedArea, selectedTurno]);
-  
-  const handleGenerateEtiqueta = () => {
-    
-  const area = areas.find(a => a.id === selectedArea)?.area;
-  const orden = ordenes.find(o => o.id === selectedOrden)?.orden;
-  const maquina = filteredMaquinas.find(m => m.id === selectedMaquina)?.maquina;
-  const producto = filteredProductos.join(', ');
-  const turno = turnos.find(t => t.id === selectedTurno)?.turno;
-  const operador = operadores.find(o => o.id === selectedTurno)?.nombreCompleto;
-  const operadorSeleccionado = operadores.find(o => o.id === selectedOperador);
 
-  console.log(area);
-  console.log(orden);
-  console.log(pesoNeto);
-  
-  // Datos a enviar a la API
-  const data = {
-    area: area || '',
-    claveProducto: producto.split(' ')[0],
-    nombreProducto: producto.split(' ').slice(1).join(' '),
-    claveOperador: operadorSeleccionado ? operadorSeleccionado.id.toString() : '',
-    operador: operadorSeleccionado ? operadorSeleccionado.nombreCompleto : '',
-    turno: turno || '',
-    pesoTarima: pesoTarima || 0,
-    pesoBruto: pesoBruto || 0,
-    pesoNeto: pesoNeto || 0,
-    piezas: piezas || 0,
-    trazabilidad: '',
-    orden: orden || '',
-    rfid: '',
-    status: 0
+  const handleOpenModal = () => {
+    const today = new Date();
+    const formattedDate = `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
+    setCurrentDate(formattedDate);
+    setOpenModal(true);
   };
 
-  // Realizar el POST a la API
-  axios.post('https://localhost:7204/api/RfidLabel', data)
-    .then(response => {
-      console.log('Etiqueta generada:', response.data);
-    })
-    .catch(error => {
-      console.error('Error generating etiqueta:', error);
-    });
-};
+  const handleCloseModal = () => setOpenModal(false);
 
+  const handleGenerateEtiqueta = () => {
+    handleOpenModal();
+  };
+
+  const handleConfirmEtiqueta = () => {
+    const area = areas.find(a => a.id === selectedArea)?.area;
+    const orden = ordenes.find(o => o.id === selectedOrden)?.orden;
+    const maquina = filteredMaquinas.find(m => m.id === selectedMaquina)?.maquina;
+    const producto = filteredProductos.join(', ');
+    const turno = turnos.find(t => t.id === selectedTurno)?.turno;
+    const operadorSeleccionado = operadores.find(o => o.id === selectedOperador);
+
+    console.log(area);
+    console.log(orden);
+    console.log(pesoNeto);
+
+    // Datos a enviar a la API
+    const data = {
+      area: area || '',
+      claveProducto: producto.split(' ')[0],
+      nombreProducto: producto.split(' ').slice(1).join(' '),
+      claveOperador: operadorSeleccionado ? operadorSeleccionado.id.toString() : '',
+      operador: operadorSeleccionado ? operadorSeleccionado.nombreCompleto : '',
+      turno: turno || '',
+      pesoTarima: pesoTarima || 0,
+      pesoBruto: pesoBruto || 0,
+      pesoNeto: pesoNeto || 0,
+      piezas: piezas || 0,
+      trazabilidad: '',
+      orden: orden || '',
+      rfid: '',
+      status: 0
+    };
+
+    // Realizar el POST a la API
+    axios.post('https://localhost:7204/api/RfidLabel', data)
+      .then(response => {
+        console.log('Etiqueta generada:', response.data);
+        handleCloseModal();
+      })
+      .catch(error => {
+        console.error('Error generating etiqueta:', error);
+      });
+  };
 
   return (
     <div className='impresion-container-bfx'>
@@ -209,16 +223,16 @@ const EtiquetadoBFX: React.FC = () => {
             ))}
           </Select>
           <Select
-          value={selectedOperador}
-          onChange={(e) => setSelectedOperador(parseInt(e.target.value as string))}
-          displayEmpty
-          fullWidth
-        >
-          <MenuItem value="" disabled>Operador</MenuItem>
-          {operadores.map(operador => (
-            <MenuItem key={operador.id} value={operador.id}>{operador.nombreCompleto}</MenuItem>
-          ))}
-        </Select>
+            value={selectedOperador}
+            onChange={(e) => setSelectedOperador(parseInt(e.target.value as string))}
+            displayEmpty
+            fullWidth
+          >
+            <MenuItem value="" disabled>Operador</MenuItem>
+            {operadores.map(operador => (
+              <MenuItem key={operador.id} value={operador.id}>{operador.nombreCompleto}</MenuItem>
+            ))}
+          </Select>
           <TextField fullWidth label="PESO BRUTO" variant="outlined" type="number" value={pesoBruto} onChange={e => setPesoBruto(parseFloat(e.target.value))} />
           <TextField fullWidth label="PESO NETO" variant="outlined" type="number" value={pesoNeto} onChange={e => setPesoNeto(parseFloat(e.target.value))} />
           <TextField fullWidth label="PESO TARIMA" variant="outlined" type="number" value={pesoTarima} onChange={e => setPesoTarima(parseFloat(e.target.value))} />
@@ -226,13 +240,75 @@ const EtiquetadoBFX: React.FC = () => {
         </Box>
         <Box className='impresion-button-bfx'>
           <Button variant="contained" className="generate-button" onClick={handleGenerateEtiqueta}>
-            GENERAR ETIQUETA
+              VISTA PREVIA
           </Button>
         </Box>
       </Box>
+      <Modal open={openModal} onClose={handleCloseModal}>
+        <Paper className="modal-content">
+          <Box className="modal-header">
+            <Typography variant="h6">Vista Previa de la Etiqueta</Typography>
+            <IconButton onClick={handleCloseModal}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+          <Box className="modal-body">
+            <div className="row">
+              <img src={EtiquetaImpresion} alt="" className="img-etiquetado" />
+            </div>
+            <div className="row">
+              <Typography><strong> PRODUCTO TERMINADO TARIMA</strong> </Typography>
+            </div>
+            <div className="row">
+              <Typography><strong>Área:</strong> {areas.find(a => a.id === selectedArea)?.area}</Typography>
+            </div>
+            <div className="row">
+              <Typography><strong>Fecha:</strong> {currentDate}</Typography>
+            </div>
+            <div className="row">
+              <Typography><strong>Producto:</strong> {filteredProductos.join(', ')}</Typography>
+            </div>
+            <div className="row"></div>
+            <div className="row">
+              <Typography><strong>Orden:</strong> {ordenes.find(o => o.id === selectedOrden)?.orden}</Typography>
+            </div>
+            <div className="row">
+              <Typography><strong>Máquina:</strong> {filteredMaquinas.find(m => m.id === selectedMaquina)?.maquina}</Typography>
+            </div>
+            <div className="row">
+              <Typography><strong>Turno:</strong> {turnos.find(t => t.id === selectedTurno)?.turno}</Typography>
+            </div>
+            <div className="row">
+              <Typography><strong>Operador:</strong> {operadores.find(o => o.id === selectedOperador)?.nombreCompleto}</Typography>
+            </div>
+            <div className="row">
+              <Typography><strong>Peso Bruto:</strong> {pesoBruto}</Typography>
+            </div>
+            <div className="row">
+                            <Typography><strong>Peso Neto:</strong> {pesoNeto}</Typography>
+            </div>
+            <div className="row">
+              <Typography><strong>Peso Tarima:</strong> {pesoTarima}</Typography>
+            </div>
+            <div className="row">
+              <Typography><strong># Piezas (Rollos, Bultos, Cajas):</strong> {piezas}</Typography>
+            </div>
+            <div className="row">
+              <Typography><strong> Codigo de Trazabilidad: </strong> {piezas}</Typography>
+            </div>
+            <div className="row">
+            <Typography><strong>OT Y/O LOTE:</strong> {ordenes.find(o => o.id === selectedOrden)?.orden}</Typography>
+            </div>
+          </Box>
+          <Box className="modal-footer">
+            <Button variant="contained" color="primary" onClick={handleConfirmEtiqueta}>
+              Guardar e Imprimir
+            </Button>
+          </Box>
+        </Paper>
+      </Modal>
     </div>
   );
 };
 
 export default EtiquetadoBFX;
-
